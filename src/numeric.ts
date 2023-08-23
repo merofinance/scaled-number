53fe03cbe77a175681ec13f094b414c0de9c94a9
chase-manning <chase@manning.dev>
2023-08-23 14:39:16 +0100

    :rocket: upgrade from BigNumber to BigInt

--- src/numeric.ts
+++ src/numeric.ts
@@ -1,50 +1,53 @@
-import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
 import { DEFAULT_DECIMALS } from './constants';
 
-export function scale(
-  number: BigNumberish,
-  decimals: number = DEFAULT_DECIMALS
-): BigNumber {
-  return BigNumber.from(number).mul(BigNumber.from(10).pow(decimals));
-}
+export const bigIntToNumber = (n: bigint): number => Number(n.toString());
 
-function flooredLog(value: BigNumber, base = 10): number {
+export const bigIntAbs = (n: bigint): bigint => (n < 0n ? -n : n);
+
+export const scale = (
+  number: number,
+  decimals: number = DEFAULT_DECIMALS
+): bigint => {
+  return BigInt(number) * BigInt(10) ** BigInt(decimals);
+};
+
+const flooredLog = (value: bigint, base = 10): number => {
   let result = 0;
-  while (value.gt(base)) {
+  while (value > BigInt(base)) {
     result++;
-    value = value.div(base);
+    value = value / BigInt(base);
   }
   return result;
-}
+};
 
-export function bigNumberToFloat(
-  value: BigNumber,
+export const bigIntToFloat = (
+  value: bigint,
   decimals: number = DEFAULT_DECIMALS,
   significantDigits = 5
-): number {
+): number => {
   const log = flooredLog(value);
   const decimalsScale = Math.min(
     Math.max(0, log - significantDigits + 1),
     decimals
   );
-  const rounded = value.div(BigNumber.from(10).pow(decimalsScale)).toNumber();
+  const rounded = bigIntToNumber(value / BigInt(10) ** BigInt(decimalsScale));
   return rounded / 10 ** Math.max(0, decimals - decimalsScale);
-}
+};
 
-function countLeadingZeros(value: number, base = 10): number {
+const countLeadingZeros = (value: number, base = 10): number => {
   let result = 0;
   while (value < 1) {
     result++;
     value *= base;
   }
   return result;
-}
+};
 
-export function floatToBigNumber(
+export const floatToBigInt = (
   value: number,
   decimals: number = DEFAULT_DECIMALS,
   significantDigits = 5
-): BigNumber {
+): bigint => {
   const leadingZeros = countLeadingZeros(value);
   const decimalScale = leadingZeros + significantDigits;
   if (decimalScale > decimals) {
@@ -52,7 +55,7 @@ export function floatToBigNumber(
   }
   const scaledSignificant = Math.round(value * 10 ** decimalScale);
   return scale(scaledSignificant, decimals - decimalScale);
-}
+};
 
 const roundToOneDp = (value: number): string => {
   return value.toLocaleString(undefined, {
@@ -107,10 +110,7 @@ export const formatCrypto = (
   });
 };
 
-export const bigNumberToString = (
-  number: BigNumber,
-  decimals: number
-): string => {
+export const bigIntToString = (number: bigint, decimals: number): string => {
   let string = number.toString();
   const prefix = string.substring(0, 1) === '-' ? '-' : '';
   string = string.replace('-', '');
@@ -121,10 +121,7 @@ export const bigNumberToString = (
   return prefix + whole + (fraction ? `.${fraction}` : '');
 };
 
-export const stringToBigNumber = (
-  value: string,
-  decimals: number
-): BigNumber => {
+export const stringToBigInt = (value: string, decimals: number): bigint => {
   if (!value || value === '.') throw new Error('errors.invalidNumber');
   value = value.replace(/,/g, '');
   const multiplier = value.substring(0, 1) === '-' ? -1 : 1;
@@ -145,9 +142,9 @@ export const stringToBigNumber = (
       fraction = fraction.substring(0, decimals) || '0';
     }
 
-    const base = BigNumber.from(10).pow(BigNumber.from(decimals));
-    return BigNumber.from(whole).mul(base).add(fraction).mul(multiplier);
+    const base = BigInt(10) ** BigInt(decimals);
+    return (BigInt(whole) * base + BigInt(fraction)) * BigInt(multiplier);
   }
-  const base = BigNumber.from(10).pow(BigNumber.from(-decimals));
-  return BigNumber.from(whole).div(base).mul(multiplier);
+  const base = BigInt(10) ** BigInt(-decimals);
+  return (BigInt(whole) / base) * BigInt(multiplier);
 };
